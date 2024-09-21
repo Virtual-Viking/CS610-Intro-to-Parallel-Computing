4f6108fbce7125ba362310bb8fa4cc0c4f3f2a2a
Virtual-Viking <abhineet.official@gmail.com>
2024-09-21 14:57:39 -0400

    project 1b

--- Assignments/a_chaudhary_P1b.c
+++ Assignments/a_chaudhary_P1b.c
@@ -2,13 +2,24 @@
 #include <math.h>
 
 int main(){
-    float n;
+    int n;
     cin>>n;
     float a = 1;
     float i = 1;
+
+    /* calculating factorial of n  */
+
     for (i = 1; i<=n; i++){
         a = a*i;
     }
-    
-    
+
+    /* setting floating point variable precision*/
+    float c = a;
+    int b = 0;
+    while (c>0.9){
+        c = c/10;
+        b++;
+    }
+    cout.precision(b);
+    cout<<a;
 }
--- test.c
+++ test.c
@@ -2,13 +2,13 @@
 #include <math.h>
 
 int main(){
-    float n;
-    cin>>n;
-    float a = 1;
-    float i = 1;
-    for (i = 1; i<=n; i++){
-        a = a*i;
+    float a = 123.0;
+    int b = 0;
+    while (a > 0.9){
+        a = a/10;
+        b++;
     }
+    cout<<b;
     
     
 }
